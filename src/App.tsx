6e087708953c6880c7cab6112c8ea7b0a892b175
Joe <joemilsom@gmail.com>
2022-12-24 10:29:50 +1300

    Setup initial Vite react project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div></div>
+      <h1>Ngahuha client</h1>
+    </div>
+  );
+}
+
+export default App;
