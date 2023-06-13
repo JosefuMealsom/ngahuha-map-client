d5221204b4a7bf48e4ed839f9e89cdbd9636b311
Josefu <joemilsom@gmail.com>
2023-06-14 11:55:09 +1200

    Setup https support for dev environment
    Why: service workers are only enabled over https, so we need this to be
    able to test the offline functionality

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/test-offline/",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      workbox: {
-        globPatterns: ["**/*"],
+  server: {
+    https: {
+      key: process.env.PRIVATE_DEV_KEY,
+      cert: process.env.PRIVATE_DEV_CERT,
     },
-    includeAssets: [
-        "**/*",
-    ],
-    }),
-  ],
+  },
+  plugins: [react(), VitePWA({})],
 });
