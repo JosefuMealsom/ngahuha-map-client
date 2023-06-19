836152834e5b0cb63b8b419780255521a997e01a
Josefu <joemilsom@gmail.com>
2023-06-19 15:42:08 +1200

    Add reuseable button component
    Why: so I don't need to redefine the button styling each time.

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,18 @@
+import { MouseEventHandler } from 'react';
+
+export function ButtonComponent(props: {
+  text: string;
+  id?: string;
+  className?: string;
+  onClickHandler?: MouseEventHandler<HTMLButtonElement>;
+}) {
+  return (
+    <button
+      id={props.id}
+      className={`${props.className} border-solid border-black border-2 p-2 hover:bg-gray-300 cursor-pointer mb-2 inline-block`}
+      onClick={props.onClickHandler}
+    >
+      {props.text}
+    </button>
+  );
+}
