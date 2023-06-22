90dc00026103a5961dd8ad9044548c03b2d19056
Josefu <joemilsom@gmail.com>
2023-06-22 14:15:52 +1200

    Fix environment variable imports in vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({
-  server: {
-    https: {
-      key: process.env.PRIVATE_DEV_KEY,
-      cert: process.env.PRIVATE_DEV_CERT,
-    },
-  },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      https: {
+        key: env.PRIVATE_DEV_KEY,
+        cert: env.PRIVATE_DEV_CERT,
       },
-    }),
-  ],
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true,
+        },
+      }),
+    ],
+  };
 });
