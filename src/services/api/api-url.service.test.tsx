4259fe90fb118e4081a2c8e58e93bd5baa15efd8
Josefu <joemilsom@gmail.com>
2023-06-23 11:50:56 +1200

    Update api url service to accept query params
    Why: so it is easier to generate well formed urls

--- src/services/api/api-url.service.test.tsx
+++ src/services/api/api-url.service.test.tsx
@@ -8,5 +8,24 @@ describe('ApiUrlService', () => {
         'https://www.dummy-api.com/joe/is/cool',
       );
     });
+
+    describe('path has query params', () => {
+      it('adds the query params to the url', async () => {
+        expect(
+          apiUrlService.getFullPath('joe/is/cool', {
+            myAwesomeKey: 'hell yeah',
+            myLameKey: 'hell no',
+          }),
+        ).toEqual(
+          'https://www.dummy-api.com/joe/is/cool?myAwesomeKey=hell+yeah&myLameKey=hell+no',
+        );
+      });
+
+      it('does not add query params if the object is empty', async () => {
+        expect(apiUrlService.getFullPath('joe/is/cool', {})).toEqual(
+          'https://www.dummy-api.com/joe/is/cool',
+        );
+      });
+    });
   });
 });
--- src/services/api/api-url.service.tsx
+++ src/services/api/api-url.service.tsx
@@ -1,8 +1,11 @@
 class ApiUrlService {
-  getFullPath(path: string) {
+  getFullPath(path: string, query?: { [propName: string]: any }) {
     const baseUrl = import.meta.env.VITE_BASE_API_URL;
     const apiUrl = new URL('/', baseUrl);
     apiUrl.pathname = path;
+    if (query) {
+      apiUrl.search = new URLSearchParams(query).toString();
+    }
     return apiUrl.toString();
   }
 }
