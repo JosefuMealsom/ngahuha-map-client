4f1b565caee1f1340bea2e4ba5994f4c08734556
Josefu <joemilsom@gmail.com>
2023-06-26 11:54:33 +1200

    Add plant and plantType tables to offline database

--- src/services/database/offline.database.tsx
+++ src/services/database/offline.database.tsx
@@ -1,20 +1,24 @@
 import Dexie, { Table } from 'dexie';
-import { PlantSite } from '../../types/api/plant-site.type';
-import { PlantSitePhoto } from '../../types/api/plant-site-photo.type';
-import { GardenArea } from '../../types/api/garden-area.type';
-import { Plant } from '../../types/api/plant.type';
+import type { PlantSite } from '../../types/api/plant-site.type';
+import type { PlantSitePhoto } from '../../types/api/plant-site-photo.type';
+import type { GardenArea } from '../../types/api/garden-area.type';
+import type { Plant } from '../../types/api/plant.type';
+import type { PlantType } from '../../types/api/plant-type.type';
 
 class OfflineDatabase extends Dexie {
   public readonly plantSite!: Table<PlantSite>;
   public readonly plantSitePhoto!: Table<PlantSitePhoto>;
   public readonly gardenArea!: Table<GardenArea>;
   public readonly plant!: Table<Plant>;
+  public readonly plantType!: Table<PlantType>;
 
   constructor() {
     super('OfflineDatabase');
     this.version(1).stores({
       gardenArea: 'id, name, updatedAt',
       species: 'id, name, updatedAt',
+      plant: 'id, species, cultivar, updatedAt',
+      plantType: 'id, name',
       plantSite: '++id, updatedAt',
       plantSitePhoto: '++id, updatedAt',
     });
@@ -26,5 +30,6 @@ const offlineDatabase = new OfflineDatabase();
 export default offlineDatabase;
 export const gardenAreaTable = offlineDatabase.gardenArea;
 export const plantTable = offlineDatabase.plant;
+export const plantTypeTable = offlineDatabase.plantType;
 export const plantSiteTable = offlineDatabase.plantSite;
 export const plantSitePhotoTable = offlineDatabase.plantSitePhoto;
