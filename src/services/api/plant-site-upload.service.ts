2fdbf3b3f7fd9578cde50cac0cce69f1f6bed205
Josefu <joemilsom@gmail.com>
2023-06-30 17:27:47 +1200

    Add service to add plants offline

---
+++ src/services/api/plant-site-upload.service.ts
@@ -0,0 +1,78 @@
+import offlineDatabase, {
+  plantSitePhotoUploadTable,
+  plantSiteUploadTable,
+  plantTable,
+} from '../offline.database';
+
+class PlantIdMissingError extends Error {
+  constructor(plantId: string) {
+    super(`Plant with id: '${plantId}' not found`);
+    this.name = 'PlantIdMissingError';
+  }
+}
+
+export const addPlantSiteWithPhoto = async (
+  photoBlob: Blob,
+  location: GeolocationCoordinates,
+  plantId: string,
+) => {
+  return new Promise(async (success, reject) => {
+    const plant = await plantTable.get(plantId);
+
+    if (!plant) {
+      reject(new PlantIdMissingError(plantId));
+    }
+
+    offlineDatabase.transaction(
+      'rw',
+      plantSiteUploadTable,
+      plantSitePhotoUploadTable,
+      async () => {
+        const plantSiteId = await addPlantSiteUpload(plantId, location);
+        await addPlantSitePhotoUpload(plantSiteId as number, photoBlob);
+
+        success(plantSiteId);
+      },
+    );
+  });
+};
+
+export const deletePlantSite = async (id: number) => {
+  offlineDatabase.transaction(
+    'rw',
+    plantSiteUploadTable,
+    plantSitePhotoUploadTable,
+    async () => {
+      const photoIds = await plantSitePhotoUploadTable
+        .where({
+          plantSiteId: id,
+        })
+        .toArray();
+
+      // Bit funky here, Dexie has issues with the id type being conditional
+      // so it always complains that I may have undefined returned.
+      const idsToDelete = photoIds.map((item) => item.id as number);
+      const filteredNumbers = idsToDelete.filter((id) => id !== undefined);
+
+      await plantSitePhotoUploadTable.bulkDelete(filteredNumbers);
+
+      await plantSiteUploadTable.delete(id);
+    },
+  );
+};
+
+function addPlantSiteUpload(plantId: string, location: GeolocationCoordinates) {
+  return plantSiteUploadTable.add({
+    plantId: plantId,
+    latitude: location.latitude,
+    longitude: location.longitude,
+    accuracy: location.accuracy,
+  });
+}
+
+async function addPlantSitePhotoUpload(plantSiteId: number, photoBlob: Blob) {
+  return await plantSitePhotoUploadTable.add({
+    plantSiteId: plantSiteId,
+    data: photoBlob,
+  });
+}
