c9eb78be17e413b50c76ef93366fea582798b195
Josefu <joemilsom@gmail.com>
2023-07-03 15:09:27 +1200

    Restrict network traffic to certain websites

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   e2e: {
     baseUrl: 'https://localhost:5173',
+    blockHosts: ['app.ngahuha-map-dev.com', 'coolupload.com'],
   },
   component: {
     devServer: {
