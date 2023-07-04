f27baa763fb75d7585cb8b989a255e3e4e0f3968
Josefu <joemilsom@gmail.com>
2023-07-04 17:52:56 +1200

    Improve general UX of uploading
    Changed it so that the less errors as a result of the UI flow will
    occur. Also it is clearer what is happening to the user.

--- src/components/PlantPhotosToUpload.tsx
+++ src/components/PlantPhotosToUpload.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ButtonComponent } from './ButtonComponent';
 import { PlantSiteComponent } from './PlantSiteComponent';
 import { useLiveQuery } from 'dexie-react-hooks';
@@ -7,16 +7,49 @@ import { uploadPlantSitesToServer } from '../services/api/sync/sync-plant-sites'
 
 export function PlantPhotosToUpload() {
   const [modalOpen, setModalState] = useState(false);
+  const [uploading, setUploadingState] = useState(false);
 
   const plantUploadCount = useLiveQuery(() => plantSiteUploadTable.count());
-
-  const plantSites = useLiveQuery(
-    () => plantSiteUploadTable.toArray(),
-    [plantUploadCount],
-  );
+  const plantSites = useLiveQuery(() => plantSiteUploadTable.toArray());
 
   function toggleModal() {
     setModalState(!modalOpen);
+  }
+
+  useEffect(() => {
+    if (plantUploadCount === 0) {
+      setModalState(false);
+    }
+  }, [plantUploadCount]);
+
+  async function uploadPlants() {
+    setUploadingState(true);
+    await uploadPlantSitesToServer();
+    setUploadingState(false);
+  }
+
+  function renderUploadButton() {
+    if (uploading || plantUploadCount === 0) return;
+
+    return (
+      <ButtonComponent
+        onClickHandler={uploadPlants}
+        className="bg-white"
+        text="Upload to server"
+      ></ButtonComponent>
+    );
+  }
+
+  function renderModalButton() {
+    if (uploading || plantUploadCount === 0) return;
+
+    return (
+      <ButtonComponent
+        onClickHandler={toggleModal}
+        className="fixed top-2 left-3 bg-white"
+        text={modalOpen ? 'Close' : 'Pending changes'}
+      ></ButtonComponent>
+    );
   }
 
   return (
@@ -26,23 +59,21 @@ export function PlantPhotosToUpload() {
           modalOpen ? '' : 'hidden'
         } mb-4 pt-14 w-full h-full absolute top-0 left-0 bg-white p-6`}
       >
-        <h1 className="font-bold mt-5 mb-3">Pending changes</h1>
-        <ButtonComponent
-          onClickHandler={uploadPlantSitesToServer}
-          className="bg-white"
-          text="Upload to server"
-        ></ButtonComponent>
+        <h1 className="font-bold mt-5 mb-3">
+          {uploading ? 'Uploading' : 'Pending changes'}
+        </h1>
+        {renderUploadButton()}
         <div>
           {plantSites?.map((plantSite) => (
-            <PlantSiteComponent key={plantSite.id} {...plantSite} />
+            <PlantSiteComponent
+              key={plantSite.id}
+              {...plantSite}
+              isUploading={uploading}
+            />
           ))}
         </div>
       </div>
-      <ButtonComponent
-        onClickHandler={toggleModal}
-        className="fixed top-2 left-3 bg-white"
-        text={modalOpen ? 'Close' : 'Pending changes'}
-      ></ButtonComponent>
+      {renderModalButton()}
     </div>
   );
 }
