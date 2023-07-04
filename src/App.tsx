b348e441fd6d11b232d77d57e64912f55e7ee53e
Josefu <joemilsom@gmail.com>
2023-07-04 17:09:37 +1200

    Add component to show status of users connection status

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import { useEffect } from 'react';
 import gardenAreaService from './services/api/garden-area.service';
 import { syncPlantsOffline } from './services/api/plant.service';
 import { syncPlantSitesOffline } from './services/api/plant-site.service';
+import { OnlineComponent } from './components/OnlineComponent';
 
 function App() {
   useEffect(() => {
-    const syncData = async () => {
-      await gardenAreaService.syncOffline();
-      await syncPlantSitesOffline();
-      await syncPlantsOffline();
-    };
-    syncData();
+    if (navigator.onLine) {
+      const syncData = async () => {
+        await gardenAreaService.syncOffline();
+        await syncPlantSitesOffline();
+        await syncPlantsOffline();
+      };
+      syncData();
+    }
   }, []);
 
   return (
     <div>
+      <OnlineComponent></OnlineComponent>
       <MapCanvas></MapCanvas>
       <PlantPhotosToUpload></PlantPhotosToUpload>
       <PlantPhotoForm></PlantPhotoForm>
