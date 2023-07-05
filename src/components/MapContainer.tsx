fcc365f77e77e372d98b793378697053b7770145
Josefu <joemilsom@gmail.com>
2023-07-05 17:04:40 +1200

    Map dom markers to canvas position
    Had to take out some logic that made zooming in the map nicer (it was
    adding a pan to the zoom), but the marker elements are lined up quite
    well. Strange things with rendering though; dom renders faster than the
    canvas, but if there is a hot reload then they line up perfectly.

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -2,17 +2,31 @@ import { useLiveQuery } from 'dexie-react-hooks';
 import { MapCanvas } from './MapCanvas';
 import { plantSiteTable } from '../services/offline.database';
 import { MapMarker } from './MapMarker';
+import { useAnimationFrame } from '../hooks/use-animation-frame.hook';
+import { PanGestureHandler } from '../services/view/pan-gesture-handler.service';
+import { ZoomGestureHandler } from '../services/view/zoom-gesture-handler.service';
+import { useMapStore } from '../store/map.store';
 
 export function MapContainer() {
   const plantSites = useLiveQuery(() => plantSiteTable.toArray());
+  const panGestureHandler = new PanGestureHandler(document.body);
+  const zoomGestureHandler = new ZoomGestureHandler(document.body);
+
+  useAnimationFrame(() => {
+    const pan = panGestureHandler.update();
+    const zoom = zoomGestureHandler.update();
+    useMapStore.getState().setZoom(zoom);
+    useMapStore.getState().setPan(pan.x, pan.y);
+  });
 
   return (
     <div>
-      <MapCanvas></MapCanvas>
-      {plantSites?.map((plantSite) => (
-        <MapMarker key={plantSite.id} {...plantSite}></MapMarker>
-      ))}
-      <></>
+      <div className="relative touch-none inline-block">
+        <MapCanvas></MapCanvas>
+        {plantSites?.map((plantSite) => (
+          <MapMarker key={plantSite.id} {...plantSite}></MapMarker>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/MapMarker.tsx
+++ src/components/MapMarker.tsx
@@ -1,13 +1,55 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { PlantSite } from '../types/api/plant-site.type';
+import { useMapStore } from '../store/map.store';
+import { interpolateToCanvasPosition } from '../services/map-position-interpolator.service';
+import { scale, translate, compose, applyToPoint } from 'transformation-matrix';
 
 export function MapMarker(props: PlantSite) {
+  const pan = useMapStore((state) => state.pan);
+  const zoom = useMapStore((state) => state.zoom);
   const marker = useRef<HTMLDivElement>(null);
+  const canvasDimensions = useMapStore((state) => state.canvasDimensions);
+  const mapBounds = useMapStore((state) => state.mapBounds);
+  const position = { latitude: props.latitude, longitude: props.longitude };
+
+  useEffect(() => {
+    const interpolatedPosition = interpolateToCanvasPosition(
+      mapBounds,
+      position,
+      canvasDimensions,
+    );
+    if (!interpolatedPosition) return;
+    if (!marker.current) return;
+
+    const height = marker.current.parentElement?.clientHeight;
+    const canvasToDomRatio =
+      (height || window.innerHeight) / canvasDimensions.height;
+
+    const x = interpolatedPosition.x;
+    const y = interpolatedPosition.y;
+
+    const { width: cw, height: ch } = canvasDimensions;
+    const offsetWidth = (cw / 2) * zoom - cw / 2;
+    const offsetHeight = (ch / 2) * zoom - ch / 2;
+
+    const transformationMatrix = compose(
+      scale(zoom, zoom),
+      scale(canvasToDomRatio, canvasToDomRatio),
+      translate(-5 / zoom, -5 / zoom),
+      translate(-offsetWidth / zoom, -offsetHeight / zoom),
+      translate(pan.x / zoom, pan.y / zoom),
+      translate(interpolatedPosition.x, interpolatedPosition.y),
+    );
+
+    const newPosition = applyToPoint(transformationMatrix, { x: 0, y: 0 });
+
+    marker.current.style.transform = `translate(${newPosition.x}px, ${newPosition.y}px)`;
+  });
 
   return (
     <div
       ref={marker}
-      className="w-4 h-4 bg-purple-500 absolute top-20 right-20 rounded-full"
+      className="w-2 h-2 bg-purple-500 absolute top-0 left-0 rounded-full opacity-60"
     ></div>
   );
 }
