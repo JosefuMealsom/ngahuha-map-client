7c9de57b5409b2c075850bd2bed1a1002f34cc85
Josefu <joemilsom@gmail.com>
2023-07-21 21:55:31 +1200

    Extract map filter and markers to another component

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,75 +1,18 @@
-import { useLiveQuery } from 'dexie-react-hooks';
 import { MapCanvas } from './MapCanvas';
-import { plantSiteTable, plantTable } from '../services/offline.database';
-import { MapMarker } from './MapMarker';
 import { useAnimationFrame } from '../hooks/use-animation-frame.hook';
 import { PanGestureHandler } from '../services/view/pan-gesture-handler.service';
 import { ZoomGestureHandler } from '../services/view/zoom-gesture-handler.service';
 import { useMapStore } from '../store/map.store';
 import { LocationMarker } from './LocationMarker';
 import { FeatureMarker } from './FeatureMarker';
-import { createRef, useEffect, useState } from 'react';
-import AutocompleteComponent from './AutocompleteComponent';
-import { PlantSite } from '../types/api/plant-site.type';
-import { getFullPlantName } from '../utils/plant-name-decorator.util';
+import { createRef, useEffect } from 'react';
+import { MapFilter } from './MapFilter';
 
 export function MapContainer() {
   const mapContainerRef = createRef<HTMLDivElement>();
-  const plantSites = useLiveQuery(() => plantSiteTable.toArray());
-  const plants = useLiveQuery(() => plantTable.toArray());
-  const [filteredPlantSites, setFilteredPlantSites] = useState<PlantSite[]>();
-  const [filterItems, setFilterItems] = useState<
-    { name: string; id: string }[]
-  >([]);
+
   let panGestureHandler: PanGestureHandler;
   let zoomGestureHandler: ZoomGestureHandler;
-
-  useEffect(() => {
-    const getPlantSites = async () =>
-      setFilteredPlantSites(await plantSiteTable.toArray());
-
-    getPlantSites();
-  }, []);
-
-  useEffect(() => {
-    if (!plants || !plantSites) return;
-    const availablePlantIds = plantSites.map((plantSite) => plantSite.plantId);
-    const filteredPlants = plants.filter((plant) =>
-      availablePlantIds.includes(plant.id),
-    );
-
-    const items = filteredPlants.map((plant) => ({
-      name: getFullPlantName(plant),
-      id: plant.id,
-    }));
-
-    setFilterItems(items);
-  }, [plants, plantSites]);
-
-  useAnimationFrame(() => {
-    const pan = panGestureHandler.update();
-    const zoom = zoomGestureHandler.update();
-    useMapStore.getState().setZoom(zoom);
-    useMapStore.getState().setPan(pan.x, pan.y);
-  });
-
-  function filterPlantSites(text: string) {
-    if (!plants || !plantSites) return;
-
-    const plantId = filterItems.find((item) => item.name === text)?.id;
-
-    const sites = plantSites.filter(
-      (plantSite) => plantSite.plantId === plantId,
-    );
-
-    setFilteredPlantSites(sites);
-  }
-
-  function resetFilter() {
-    if (!plantSites) return;
-
-    setFilteredPlantSites(plantSites);
-  }
 
   useEffect(() => {
     if (!mapContainerRef.current) return;
@@ -78,13 +21,17 @@ export function MapContainer() {
     zoomGestureHandler = new ZoomGestureHandler(mapContainerRef.current);
   }, []);
 
+  useAnimationFrame(() => {
+    const pan = panGestureHandler.update();
+    const zoom = zoomGestureHandler.update();
+    useMapStore.getState().setZoom(zoom);
+    useMapStore.getState().setPan(pan.x, pan.y);
+  });
+
   return (
     <div ref={mapContainerRef} className="h-screen overflow-hidden w-full">
       <div className="relative touch-none inline-block select-none overflow-hidden">
         <MapCanvas />
-        {filteredPlantSites?.map((plantSite) => (
-          <MapMarker key={plantSite.id} {...plantSite} />
-        ))}
         <LocationMarker />
         <FeatureMarker
           text="The steppes"
@@ -94,15 +41,7 @@ export function MapContainer() {
           text="The avocado orchard"
           position={{ latitude: -35.377025, longitude: 173.965264 }}
         />
-      </div>
-      <div className="absolute top-16 left-0 px-5 flex w-full justify-center max-w-md">
-        <AutocompleteComponent
-          placeholder="Filter plant sites"
-          items={filterItems.map((item) => item.name)}
-          suggestionText="Available"
-          onItemSelectHandler={filterPlantSites}
-          onClearHandler={resetFilter}
-        />
+        <MapFilter />
       </div>
     </div>
   );
---
+++ src/components/MapFilter.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import { PlantSite } from '../types/api/plant-site.type';
+import AutocompleteComponent from './AutocompleteComponent';
+import { useLiveQuery } from 'dexie-react-hooks';
+import { plantSiteTable, plantTable } from '../services/offline.database';
+import { getFullPlantName } from '../utils/plant-name-decorator.util';
+import { MapMarker } from './MapMarker';
+
+export function MapFilter() {
+  const plantSites = useLiveQuery(() => plantSiteTable.toArray());
+  const plants = useLiveQuery(() => plantTable.toArray());
+  const [filteredPlantSites, setFilteredPlantSites] = useState<PlantSite[]>();
+  const [filterItems, setFilterItems] = useState<
+    { name: string; id: string }[]
+  >([]);
+
+  useEffect(() => {
+    const getPlantSites = async () =>
+      setFilteredPlantSites(await plantSiteTable.toArray());
+
+    getPlantSites();
+  }, []);
+
+  useEffect(() => {
+    if (!plants || !plantSites) return;
+    const availablePlantIds = plantSites.map((plantSite) => plantSite.plantId);
+    const filteredPlants = plants.filter((plant) =>
+      availablePlantIds.includes(plant.id),
+    );
+
+    const items = filteredPlants.map((plant) => ({
+      name: getFullPlantName(plant),
+      id: plant.id,
+    }));
+
+    setFilterItems(items);
+  }, [plants, plantSites]);
+
+  function filterPlantSites(text: string) {
+    if (!plants || !plantSites) return;
+
+    const plantId = filterItems.find((item) => item.name === text)?.id;
+
+    const sites = plantSites.filter(
+      (plantSite) => plantSite.plantId === plantId,
+    );
+
+    setFilteredPlantSites(sites);
+  }
+
+  function resetFilter() {
+    if (!plantSites) return;
+
+    setFilteredPlantSites(plantSites);
+  }
+
+  return (
+    <div>
+      {filteredPlantSites?.map((plantSite) => (
+        <MapMarker key={plantSite.id} {...plantSite} />
+      ))}
+      <div className="absolute left-1/2 -translate-x-1/2 top-16 flex w-full max-w-md">
+        <AutocompleteComponent
+          placeholder="Filter plant sites"
+          items={filterItems.map((item) => item.name)}
+          suggestionText="Available"
+          onItemSelectHandler={filterPlantSites}
+          onClearHandler={resetFilter}
+        />
+      </div>
+    </div>
+  );
+}
