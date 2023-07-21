c6d0d5a1d19278502153baa47e81d467c04fb799
Josefu <joemilsom@gmail.com>
2023-07-22 10:51:59 +1200

    Add toast library
    Can decide whether to keep later, but for now makes everything easier

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { MapCanvas } from './components/MapCanvas';
 import { PlantPhotosToUpload } from './components/PlantPhotosToUpload';
 import { PlantPhotoForm } from './components/PlantPhotoForm';
-import { useEffect } from 'react';
 import gardenAreaService from './services/api/garden-area.service';
 import { syncPlantsOffline } from './services/api/plant.service';
 import { syncPlantSitesOffline } from './services/api/plant-site.service';
 import { MapContainer } from './components/MapContainer';
 import { ClosestPlantsToUser } from './components/ClosestPlantsToUser';
 import { syncPlantSitePhotosOffline } from './services/api/plant-site-photo.service';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+if (navigator.onLine) {
+  const syncData = async () => {
+    await gardenAreaService.syncOffline();
+    await syncPlantsOffline();
+    await syncPlantSitesOffline();
+    await syncPlantSitePhotosOffline();
+  };
+  syncData();
+}
 
 function App() {
-  useEffect(() => {
-    if (navigator.onLine) {
-      const syncData = async () => {
-        await gardenAreaService.syncOffline();
-        await syncPlantsOffline();
-        await syncPlantSitesOffline();
-        await syncPlantSitePhotosOffline();
-      };
-      syncData();
-    }
-  }, []);
-
   return (
     <div>
-      <MapContainer></MapContainer>
-      <PlantPhotosToUpload></PlantPhotosToUpload>
-      <PlantPhotoForm></PlantPhotoForm>
-      <ClosestPlantsToUser></ClosestPlantsToUser>
+      <MapContainer />
+      <PlantPhotosToUpload />
+      <PlantPhotoForm />
+      <ClosestPlantsToUser />
+      <ToastContainer />
     </div>
   );
 }
