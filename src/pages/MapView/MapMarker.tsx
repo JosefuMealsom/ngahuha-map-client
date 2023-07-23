d699393f04ae1d25b9f84249f0757973959b0690
Josefu <joemilsom@gmail.com>
2023-07-23 12:32:42 +1200

    Optimise marker drawing
    Why: Think I was calling twice the amount of render calls as I needed,
    draw it in a anim frame loop instead

--- src/pages/MapView/MapMarker.tsx
+++ src/pages/MapView/MapMarker.tsx
@@ -1,17 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { PlantSite } from '../../types/api/plant-site.type';
 import { interpolateToDomPosition } from '../../services/map-position-interpolator.service';
 
 import pinSvg from '../../assets/svg/map-pin.svg';
 import { useMapStore } from '../../store/map.store';
+import { useAnimationFrame } from '../../hooks/use-animation-frame.hook';
 
 export function MapMarker(props: PlantSite) {
   const marker = useRef<HTMLDivElement>(null);
   const position = { latitude: props.latitude, longitude: props.longitude };
-  const zoom = useMapStore((state) => state.zoom);
-  const pan = useMapStore((state) => state.pan);
 
-  useEffect(() => {
+  useAnimationFrame(() => {
     if (!marker.current) return;
 
     const newPosition = interpolateToDomPosition(
@@ -24,13 +23,13 @@ export function MapMarker(props: PlantSite) {
 
     marker.current.classList.remove('hidden');
     marker.current.style.transform = `translate(${newPosition.x}px, ${newPosition.y}px)`;
-  }, [zoom, pan]);
+  });
 
   return (
     <div
       id={props.id}
       ref={marker}
-      className="w-9 fill-white absolute -top-4 -left-2 rounded-full hidden"
+      className="w-9 fill-white absolute -top-4 -left-2 rounded-full hidden will-change-transform"
       data-cy="map-marker"
     >
       <img src={pinSvg} className="select-none pointer-events-none" />
