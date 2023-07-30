e3de4035fd63e9dc3b2f098256b4ec7299c933ca
Josefu <joemilsom@gmail.com>
2023-07-30 14:45:53 +1200

    Implement router navigation for pending uploads page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Outlet } from 'react-router-dom';
 import { ButtonComponent } from './components/ButtonComponent';
 import { LinkComponent } from './components/LinkComponent';
+import { useLiveQuery } from 'dexie-react-hooks';
+import { plantSiteUploadTable } from './services/offline.database';
 
 if (navigator.onLine) {
   const syncData = async () => {
@@ -23,19 +25,34 @@ if (navigator.onLine) {
 }
 
 function App() {
+  const plantUploadCount = useLiveQuery(() => plantSiteUploadTable.count());
+
+  function renderPendingUploadLink() {
+    if (plantUploadCount === 0) return;
+
+    return (
+      <div data-cy="open-upload-form" className="fixed left-5 top-5">
+        <LinkComponent
+          link="/plant-site/pending-upload"
+          text="Pending upload"
+        />
+      </div>
+    );
+  }
+
   return (
     <div>
       <MapContainer />
-      <PlantPhotosToUpload />
-      <ToastContainer />
-      <div data-cy="open-closest-plants" className="fixed bottom-5 left-5 z-0">
+      <div data-cy="open-closest-plants" className="fixed bottom-5 left-5">
         <LinkComponent link="/closest-plants" text="Closest plants" />
       </div>
-      <div data-cy="open-plant-form" className="fixed right-5 bottom-5 z-0">
+      <div data-cy="open-plant-form" className="fixed right-5 bottom-5">
         <LinkComponent link="/plant-site/new" text="New plant site" />
       </div>
+      {renderPendingUploadLink()}
 
       <Outlet />
+      <ToastContainer />
     </div>
   );
 }
