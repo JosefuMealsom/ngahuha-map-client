bc52ade144831603d42291111b2658b8a82f93cb
Josefu <joemilsom@gmail.com>
2023-07-30 21:54:28 +1200

    Add carousel component to view images

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { PlantPhotosToUpload } from './pages/PendingUploads/PlantPhotosToUpload';
-import { PlantPhotoForm } from './pages/NewPlantSite/PlantPhotoForm';
 import gardenAreaService from './services/api/garden-area.service';
 import { syncPlantsOffline } from './services/api/plant.service';
 import { syncPlantSitesOffline } from './services/api/plant-site.service';
 import { MapContainer } from './pages/MapView/MapContainer';
-import { ClosestPlantsToUser } from './pages/ClosestPlants/ClosestPlantsToUser';
 import { syncPlantSitePhotosOffline } from './services/api/plant-site-photo.service';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Outlet } from 'react-router-dom';
-import { ButtonComponent } from './components/ButtonComponent';
 import { LinkComponent } from './components/LinkComponent';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { plantSiteUploadTable } from './services/offline.database';
