3f7683ebe4cff42a7193ff8ec7e859b1780e6236
Josefu <joemilsom@gmail.com>
2023-07-30 13:27:37 +1200

    Remove unused code

--- src/pages/PendingUploads/PlantSiteComponent.tsx
+++ src/pages/PendingUploads/PlantSiteComponent.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from 'react';
-import type { Plant } from '../../types/api/plant.type';
 import type { PlantSiteUpload } from '../../types/api/upload/plant-site-upload.type';
-import { plantTable } from '../../services/offline.database';
 import { getFullPlantName } from '../../utils/plant-name-decorator.util';
 import { deletePlantSite } from '../../services/api/plant-site-upload.service';
 import trashSvg from '../../assets/svg/trash-2.svg';
