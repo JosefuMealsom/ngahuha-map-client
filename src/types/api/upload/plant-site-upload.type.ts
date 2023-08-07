4b756cc0e3bce191dc120edff9e08f770eeb4b7a
Josefu <joemilsom@gmail.com>
2023-08-07 22:23:40 +1200

    Simplify plantSiteUpload by removing separate photo model
    Why: way easier to store the photos on the plantSiteUpload, as don't
    have to do a whole bunch of asynchronous queries to get the photos.
    
    May be worthwhile to do the same for the plantSites that have been
    saved.

--- src/types/api/upload/plant-site-upload.type.ts
+++ src/types/api/upload/plant-site-upload.type.ts
@@ -1,7 +1,13 @@
+export type PlantSitePhotoUpload = {
+  data: ArrayBuffer;
+  blobKey?: string;
+};
+
 export type PlantSiteUpload = {
   id?: number;
   plantId?: string;
   latitude: number;
   longitude: number;
   accuracy: number;
+  photos: PlantSitePhotoUpload[];
 };
