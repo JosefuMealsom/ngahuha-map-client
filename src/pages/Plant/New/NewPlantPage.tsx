ceea00ade8b3d343839f164bc20ce94a7906baa5
Josefu <joemilsom@gmail.com>
2023-08-27 19:52:58 +1200

    Fix padding on create a new plant page

--- src/pages/Plant/New/NewPlantPage.tsx
+++ src/pages/Plant/New/NewPlantPage.tsx
@@ -24,8 +24,8 @@ export function NewPlantPage() {
   }
 
   return (
-    <div className="absolute top-0 pt-7 left-0 bg-background w-full h-full">
-      <div className="w-full bg-background px-6">
+    <div className="absolute top-0 pt-safe left-0 bg-background w-full h-full">
+      <div className="w-full bg-background px-6 pt-7">
         <h1 className="font-bold mb-7 text-xl text-inverted-background">
           Create a new plant
         </h1>
