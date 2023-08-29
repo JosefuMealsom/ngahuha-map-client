9a24db7fc515aeb1a0a351fba4647a39e41b7dfb
Josefu <joemilsom@gmail.com>
2023-08-29 20:53:36 +1200

    Fix tsconfig build

--- src/components/MarkdownEditorComponent.cy.tsx
+++ src/components/MarkdownEditorComponent.cy.tsx
@@ -4,7 +4,7 @@ describe('<MarkdownEditorComponent />', () => {
   it('displays the passed in value in the content', () => {
     cy.mount(
       <MarkDownEditorComponent
-        onSaveHandler={() => {}}
+        onChangeHandler={() => {}}
         value="Wow!! Really interesting."
       />,
     );
@@ -15,14 +15,12 @@ describe('<MarkdownEditorComponent />', () => {
   it('can edit and on save triggers a passed in callback', () => {
     const onSave = cy.spy();
 
-    cy.mount(<MarkDownEditorComponent onSaveHandler={onSave} />);
+    cy.mount(<MarkDownEditorComponent onChangeHandler={onSave} />);
 
     cy.get('[data-cy="markdown-toggle-edit"]').click();
 
-    cy.get('[data-cy="markdown-content-input"]').type('hello');
-
-    cy.get('[data-cy="markdown-save-button"]')
-      .click()
+    cy.get('[data-cy="markdown-content-input"]')
+      .type('hello')
       .then(() => {
         expect(onSave).to.have.been.calledWith('hello');
       });
