15e817b2974abcb239fae20425fd308024920b12
Josefu <joemilsom@gmail.com>
2023-08-29 12:35:08 +1200

    Disable cache control for presigned put requests

--- src/services/api/sync/blob-uploader.service.ts
+++ src/services/api/sync/blob-uploader.service.ts
@@ -3,7 +3,13 @@ import axiosClient from '../../axios/axios-client';
 type BlobUrl = { blobKey: string; url: string };
 
 export const fetchBlobUploadUrl = async (): Promise<BlobUrl> => {
-  const response = await axiosClient.get('blob/presigned-upload-url');
+  const response = await axiosClient.get('blob/presigned-upload-url', {
+    headers: {
+      'Cache-Control': 'no-cache',
+      Pragma: 'no-cache',
+      Expires: '0',
+    },
+  });
 
   return response.data;
 };
