48e9ad9f29026bf5727e1b256590ff314b01341b
Josefu <joemilsom@gmail.com>
2023-08-30 10:17:47 +1200

    Make navigation bar scrollable
    Why: if we decide to add more options, we can scroll it

--- src/pages/Navigation/CreateNavigationBar.tsx
+++ src/pages/Navigation/CreateNavigationBar.tsx
@@ -4,30 +4,32 @@ export function CreateNavigationBar(props: {
   activePage: 'New plant site' | 'Create plant' | 'Create feature';
 }) {
   return (
-    <div className="flex pt-4 ml-6">
-      <div className="mr-1" data-cy="open-plant-form">
-        <ActiveFilterLinkComponent
-          text="New plant site"
-          link="/plant-site/new"
-          active={props.activePage === 'New plant site'}
-          replace={true}
-        />
-      </div>
-      <div className="mr-1" data-cy="new-plant-form">
-        <ActiveFilterLinkComponent
-          text="New plant"
-          link="/plants/new"
-          active={props.activePage === 'Create plant'}
-          replace={true}
-        />
-      </div>
-      <div className="mr-1" data-cy="new-feature-form">
-        <ActiveFilterLinkComponent
-          text="New feature"
-          link="/feature/new"
-          active={props.activePage === 'Create feature'}
-          replace={true}
-        />
+    <div className="overflow-x-scroll hide-scrollbar">
+      <div className="flex pt-4 pl-6 w-max pr-6">
+        <div className="mr-1" data-cy="open-plant-form">
+          <ActiveFilterLinkComponent
+            text="New plant site"
+            link="/plant-site/new"
+            active={props.activePage === 'New plant site'}
+            replace={true}
+          />
+        </div>
+        <div className="mr-1" data-cy="new-plant-form">
+          <ActiveFilterLinkComponent
+            text="New plant"
+            link="/plants/new"
+            active={props.activePage === 'Create plant'}
+            replace={true}
+          />
+        </div>
+        <div className="mr-1" data-cy="new-feature-form">
+          <ActiveFilterLinkComponent
+            text="New feature"
+            link="/feature/new"
+            active={props.activePage === 'Create feature'}
+            replace={true}
+          />
+        </div>
       </div>
     </div>
   );
