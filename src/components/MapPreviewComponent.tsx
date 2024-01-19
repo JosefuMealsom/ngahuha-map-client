b4e360fbcb67d9f17b8a466f16824c54c39350d4
Josefu <joemilsom@gmail.com>
2024-01-19 14:23:44 +1300

    Add map preview class on plant site and plants pages

---
+++ src/components/MapPreviewComponent.tsx
@@ -0,0 +1,27 @@
+import { LocationMarker } from '../pages/MapView/LocationMarker';
+import { MapMarker } from '../pages/MapView/MapMarker';
+import { MapSvg } from '../pages/MapView/MapSvg';
+import { PlantSite } from '../types/api/plant-site.type';
+import { PanZoomComponent } from './PanZoomComponent';
+
+export const MapPreviewComponent = (props: { plantSites: PlantSite[] }) => {
+  return (
+    <div className="p-1 rounded-lg overflow-hidden">
+      <div className="relative w-full overflow-hidden pb-10 bg-white h-96">
+        <PanZoomComponent
+          className="w-full bg-[#96AF98]"
+          zoom={0.7}
+          pan={{ x: -250, y: -600 }}
+          minZoom={0.7}
+        >
+          <MapSvg>
+            <LocationMarker />
+            {props.plantSites.map((plantSite) => (
+              <MapMarker key={plantSite.id} {...plantSite} active={false} />
+            ))}
+          </MapSvg>
+        </PanZoomComponent>
+      </div>
+    </div>
+  );
+};
