9cfb594bfcee1fe4db37433f7c37b90494642531
Josefu <joemilsom@gmail.com>
2024-01-23 12:52:39 +1300

    Remove duplicate component logic
    Why: there were 2 list components that were doing the same thing and
    displaying the same data. Refactor to use 1 of them.

--- src/pages/PlantSite/PlantSiteListItemComponent.tsx
+++ src/pages/PlantSite/PlantSiteListItemComponent.tsx
@@ -1,40 +1,23 @@
-import { PlantSite } from '../../types/api/plant-site.type';
-import { useEffect, useState } from 'react';
-import { plantSitePhotoTable } from '../../services/offline.database';
-import blobToDataUrlService from '../../services/blob-to-data-url.service';
-import { usePlant } from '../../hooks/use-plant.hook';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { usePlant } from '../../hooks/use-plant.hook';
+import { useIsInViewport } from '../../hooks/use-is-in-viewport.hook';
+import { useDisplayPhoto } from '../../hooks/use-display-photo.hook';
+import { PlantSite } from '../../types/api/plant-site.type';
 import { PlantTitleComponent } from '../../components/PlantTitleComponent';
 
 export function PlantSiteListItemComponent(props: PlantSite) {
   const plant = usePlant(props.plantId);
-  const [photoDataUrl, setPhotoDataUrl] = useState<string>();
-
-  const getPlantInfo = async () => {
-    const firstPlantSitePhotos = await plantSitePhotoTable
-      .where({ plantSiteId: props.id })
-      .toArray();
-
-    let primaryPhoto = firstPlantSitePhotos.find(
-      (photo) => photo.primaryPhoto === true,
-    );
-
-    if (!primaryPhoto) {
-      primaryPhoto = firstPlantSitePhotos[0];
-    }
-
-    if (!primaryPhoto || !primaryPhoto?.data) return;
-
-    const photoData = await blobToDataUrlService.convert(
-      new Blob([primaryPhoto.data]),
-    );
-
-    setPhotoDataUrl(photoData || '');
-  };
+  const [previewImage, setPreviewImage] = useState<string>();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inViewport = useIsInViewport(containerRef);
+  const displayPhoto = useDisplayPhoto(props.id);
 
   useEffect(() => {
-    getPlantInfo();
-  }, []);
+    if (!displayPhoto || !inViewport) return;
+
+    setPreviewImage(displayPhoto.dataUrl);
+  }, [displayPhoto, inViewport]);
 
   function renderPlantTitle() {
     if (!plant) return;
@@ -42,27 +25,24 @@ export function PlantSiteListItemComponent(props: PlantSite) {
     return <PlantTitleComponent {...plant} />;
   }
 
-  function renderPlantInfo() {
-    return (
+  return (
+    <Link to={`/plant-site/${props.id}`}>
       <div
-        className="h-96 cursor-pointer sm:hover:opacity-90 bg-white"
+        className={`h-96 cursor-pointer sm:hover:opacity-90 bg-white`}
         data-cy={`closest-plant-site-${props.id}`}
+        ref={containerRef}
       >
         <div className="w-full h-full align-top relative">
           <img
-            src={photoDataUrl}
-            className="w-full h-full object-cover min-h-[15rem]"
+            src={previewImage}
+            className={`w-full h-full relative object-cover ${
+              previewImage ? 'opacity-100' : 'opacity-0'
+            } transition-opacity duration-300`}
           />
 
           <div className="absolute top-0">{renderPlantTitle()}</div>
         </div>
       </div>
-    );
-  }
-
-  return (
-    <Link to={`/plant-site/${props.id}`}>
-      <div className="w-full">{renderPlantInfo()}</div>
     </Link>
   );
 }
