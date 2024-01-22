36c9dee153167069c94fe3e69d72513c4bf1cf77
Josefu <joemilsom@gmail.com>
2024-01-22 20:05:32 +1300

    Remove syncing of features from the api
    Why: it is not so useful as a 'feature' at this point.

--- src/services/api/sync-data.service.ts
+++ src/services/api/sync-data.service.ts
@@ -1,15 +1,10 @@
 import { useAppStore } from '../../store/app.store';
-import { syncFeaturesOffline } from './feature/feature.service';
 import {
   syncPlantSitePhotoFilesOffline,
   syncPlantSitePhotosOffline,
 } from './plant-site/plant-site-photo.service';
 import { syncPlantSitesOffline } from './plant-site/plant-site.service';
 import { syncPlantsOffline } from './plant.service';
-import {
-  syncFeaturePhotoFilesOffline,
-  syncFeaturePhotosOffline,
-} from './feature/feature-photo.service';
 
 export const syncDataFromServer = async () => {
   if (!navigator.onLine) return;
@@ -21,11 +16,8 @@ export const syncDataFromServer = async () => {
     await syncPlantsOffline();
     await syncPlantSitesOffline();
     await syncPlantSitePhotosOffline();
-    await syncFeaturesOffline();
-    await syncFeaturePhotosOffline();
     setSyncStatus('Syncing photos');
     await syncPlantSitePhotoFilesOffline();
-    await syncFeaturePhotoFilesOffline();
     setSyncStatus('Not syncing');
   } catch (error) {
     setSyncStatus('Not syncing');
