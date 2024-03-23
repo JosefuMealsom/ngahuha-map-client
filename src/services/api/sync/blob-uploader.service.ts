d16133988edadd69dfaa957f412500ccfa944301
Josefu <joemilsom@gmail.com>
2024-03-23 19:36:51 +1300

    Add additional query param to help bust cache

--- src/services/api/sync/blob-uploader.service.ts
+++ src/services/api/sync/blob-uploader.service.ts
@@ -7,7 +7,7 @@ type BlobUrl = { blobKey: string; url: string };
 export const fetchBlobUploadUrl = async (): Promise<BlobUrl> => {
   // Timestamp required to prevent axios from caching get request
   const response = await axiosClient.get(
-    `blob/presigned-upload-url?timestamp=${Date.now()}`,
+    `blob/presigned-upload-url?timestamp=${Date.now()}&rid=${crypto.randomUUID()}`,
   );
   return response.data;
 };
